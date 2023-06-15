df8e1109ed0d246819513b6b543776dd84e4059a
Dipakkkk <dr916139@gmail.com>
2023-06-15 17:00:03 +0530

    back to top add

--- assets/js/style.js
+++ assets/js/style.js
@@ -62,3 +62,18 @@ $(".slick-slider2").slick({
 
   // arrows: false, Boolean
 });
+
+var btn = $("#button");
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass("show");
+  } else {
+    btn.removeClass("show");
+  }
+});
+
+btn.on("click", function (e) {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: 0 }, "300");
+});
